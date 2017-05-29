3b0fa887d324d988a1369aa6135fee2ad5fa1b75
MBaran88 <zdzibq@gmail.com>
2017-05-29 18:14:29 +0200

    Commit #1

--- src/com/calc/Main.java
+++ src/com/calc/Main.java
@@ -1,10 +1,46 @@
 package com.calc;
 
+import javafx.scene.web.PromptData;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// zrobic prosty kalkultor ktory przy uzyciu metody Scanner daje nam mozliwosc dodawania, odejmowania, mnozenia i dzielenia
+       /* Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int suma = (x + y);
+       System.out.println(suma);*/
+       double n1, n2;
+       String operation;
+       Scanner scannerObject = new Scanner(System.in);
+        System.out.println("Enter first number");
+        n1 = scannerObject.nextDouble();
 
-        
+        System.out.println("Enter second number");
+        n2 = scannerObject.nextDouble();
+
+        Scanner op = new Scanner(System.in);
+        System.out.println("Enter your operation");
+        operation = op.next();
+
+        switch (operation){
+            case "+":
+                System.out.println("Your answer is: " + (n1+n2));
+                break;
+            case "-":
+                System.out.println("Your answer is: " + (n1-n2));
+                break;
+            case "*":
+                System.out.println("Your answer is: " + (n1*n2));
+                break;
+            case "/":
+                System.out.println("Your answer is: " + (n1/n2));
+                break;
+            default:
+                System.out.println("Ich verstehe nicht");
+        }
     }
 }
