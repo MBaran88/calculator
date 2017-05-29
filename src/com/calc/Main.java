e5197b9e8d5fd55c989604af5f3474f28381b5bf
MBaran88 <zdzibq@gmail.com>
2017-05-29 17:53:00 +0200

    Commit #1

---
+++ src/com/calc/Main.java
@@ -0,0 +1,10 @@
+package com.calc;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// zrobic prosty kalkultor ktory przy uzyciu metody Scanner daje nam mozliwosc dodawania, odejmowania, mnozenia i dzielenia
+
+        
+    }
+}
